4d833678d9d69f121a6d30eb4e6e658dcc744ed1
Ryan Heldt <ryan@ryanheldt.com>
2024-04-07 19:56:47 -0500

    Base UI/UX libraries.

--- Worshipmaker.Web/wwwroot/scripts/site.js
+++ Worshipmaker.Web/wwwroot/scripts/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/* global bootstrap: false */
+(() => {
+    'use strict'
+    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(tooltipTriggerEl => {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
